9e0c8934efccdedaf7e02569c0cc85cdbdbea744
Nicola <kolya.vladimirsky@gmail.com>
2021-01-06 20:06:43 +0300

    first

---
+++ Recognition.cpp
@@ -0,0 +1,135 @@
+#include <ctime>
+#include "Recognition.h"
+
+Recognition::Recognition(const char *directoryPath, const char *noisyPath) {
+    loadTemplates(directoryPath);
+    this->X = MatrixClass(getVectorFromFile(noisyPath));
+    std::cout << "input vector: " << std::endl;
+    X.show();
+    std::cout << "Image to recognize: " << std::endl;
+    X.beautifulVisualization(N);
+    this->W = MatrixClass(N , N);
+    showImages();
+    calculateWeights();
+    recognition();
+}
+
+void Recognition::loadTemplates(const char *directoryPath) {
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (directoryPath)) != NULL) {
+        while ((ent = readdir (dir)) != NULL) {
+            std::string file = ent->d_name;
+            if (file.substr(file.find_last_of('.') + 1) == TXT_EXTENSION) {
+                std::string filePath = directoryPath;
+                filePath += "/" + file;
+                std::vector<double> tmp = getVectorFromFile(filePath.c_str());
+                MatrixClass pattern(tmp);
+                templates.push_back(pattern);
+            }
+        }
+        closedir (dir);
+    } else {
+        perror ("could not open directory");
+        exit(EXIT_FAILURE);
+    }
+}
+
+std::vector<double> Recognition::getVectorFromFile(const char *file) {
+    std::vector<double> vector;
+    std::ifstream input(file);
+    if (!input.is_open()) {
+        std::logic_error wrongFileName("File reading error");
+        exit(EXIT_FAILURE);
+    } else {
+        double number;
+        while (input >> number) {
+            vector.push_back(number);
+        }
+        input.close();
+    }
+    return vector;
+}
+
+void Recognition::calculateWeights() {
+    double e = 0.000000001;
+    double h = 0.8;
+    double change;
+    std::vector<double> change_;
+    iterations = 0;
+    do {
+        iterations++;
+        change = 0;
+        for (MatrixClass image : templates) {
+            MatrixClass Xi = image.transpose();
+            MatrixClass deltaW = (Xi - W * Xi ) * Xi.transpose()  * (h / N);
+            W = W + deltaW;
+            change += deltaW.sumABS();
+        }
+        change_.push_back(change);
+        if (change_.size() > 1) {
+            change = abs(change - change_[change_.size() - 2]);
+        }
+    } while (change > e);
+    std::cout << "Iteration: " << iterations << std::endl;
+}
+
+void Recognition::recognition() {
+    bool relaxation = false;
+    MatrixClass prev;
+    generateRandomIndexes();
+    recIterations = 0;
+    do {
+        doIteration(X);
+        if (prev == X) {
+            relaxation = true;
+        } else {
+            prev = X;
+        }
+        recIterations++;
+    } while (!relaxation);
+    showAnswer(recIterations);
+}
+
+void Recognition::doIteration(MatrixClass &X) {
+    int changed = 0;
+    do {
+        unsigned int index = getRandomIndex(changed + 1);
+        double newXi = 0;
+        for (unsigned int i = 0; i < N; i++){
+            newXi += X(0, i) * W(index, i);
+        }
+//        MatrixClass Xi = X * W;
+//        X(0, index) = Xi(0, index);
+        X(0, index) = newXi;
+        X.activationFunction();
+        changed++;
+    }while (changed < N);
+}
+
+void Recognition::showAnswer(int iteration) {
+    printf("You got static attractor!\n");
+    std::cout << "After " << iteration << " iteration(s) recognized image is: " << std::endl;
+    X.beautifulVisualization(N);
+    std::cout << "Output vector is: "; X.show();
+}
+
+void Recognition::showImages() {
+    std::cout << "Images-templates are: " << std::endl;
+    for (MatrixClass image : templates) {
+        image.beautifulVisualization(N);
+        std::cout << std::endl;
+    }
+}
+
+void Recognition::generateRandomIndexes() {
+    srand(std::time(nullptr));
+    for(unsigned int i = 0; i < N; i ++) {
+        randomIndexes.push_back(i);
+    }
+    std::shuffle(randomIndexes.begin(), randomIndexes.end(), std::mt19937(std::random_device()()));
+}
+
+unsigned int Recognition::getRandomIndex(int i) {
+    return randomIndexes[randomIndexes.size() - i];
+}
---
+++ Recognition.h
@@ -0,0 +1,39 @@
+//Vladimirsky Nicolai, group 821703
+#ifndef RECOGNITION_RECOGNITION_H
+#define RECOGNITION_RECOGNITION_H
+#include <fstream>
+#include <algorithm>
+#include <dirent.h>
+#include <random>
+#include "MatrixClass.h"
+#define N   49
+#define TXT_EXTENSION "txt"
+
+
+class Recognition {
+public:
+    Recognition(){}
+    Recognition(const char *directoryPath, const char *noisyPath);
+
+private:
+    std::vector<double> getVectorFromFile(const char *file);
+    void loadTemplates(const char *directoryPath);
+    void calculateWeights();
+    void recognition();
+    void showImages();
+    void showAnswer(int i);
+    void generateRandomIndexes();
+    unsigned int getRandomIndex(int index);
+    void doIteration(MatrixClass &X);
+
+    std::vector<MatrixClass> templates;
+    std::vector<unsigned int> randomIndexes;
+    MatrixClass X;
+    MatrixClass W;
+    int iterations;
+    int recIterations;
+
+};
+
+
+#endif //RECOGNITION_RECOGNITION_H
